b48f183ad06817035c79a0d5c65cbecd29d61574
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-07-05 18:55:14 -0300

    adicionando componente Header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1> Pokedex </h1>
       <BrowserRouter>
+        <Header />
+        <h1> Pokedex </h1>
         <Route path="/" render={(props ) => <Pokedex 
         {...props} 
         pokemons={pokemons} /> } 
